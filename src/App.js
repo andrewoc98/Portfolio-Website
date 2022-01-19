cd46e588426a1d51ce4409fcdee987d96630618d
andrewoc98 <60736542+andrewoc98@users.noreply.github.com>
2022-01-19 13:17:42 +0000

    added moon offset, more stars, sun rotation,clouds

--- src/App.js
+++ src/App.js
@@ -2,36 +2,46 @@ import ReactDOM from 'react-dom'
 import React, { useRef, useState, useEffect,Suspense  } from 'react'
 import { Canvas, useFrame ,useLoader } from '@react-three/fiber'
 import * as THREE from 'three';
-import { OrbitControls, useTexture } from "@react-three/drei";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
 import Color from './Images/Color.jpg'
 import ao from './Images/3tqI6.jpg'
-import MoonColor from './Images/moon.jpg';
 import displacement from './Images/displacement_Map.jpg'
 import moonColor from './Images/moon.jpg'
-import moonRough from './Images/WAC_GLD100_E000N1800_004P-1024x512.jpg'
+import CloudTexture from './Images/earthCloud.png'
 
+
+function Clouds(){
+  var gcloud = new THREE.SphereGeometry(2.1,32,32)
+  var mcloud = new THREE.MeshPhongMaterial({map:THREE.ImageUtils.loadTexture(CloudTexture),transparent:true});
+  var cloudMesh = new THREE.Mesh(gcloud,mcloud);
+  useFrame(()=>{
+    cloudMesh.rotateY(0.002)
+    cloudMesh.rotateX(0.001)
+  })
+  return(
+    <primitive object={cloudMesh}/>
+  );
+}
 
 function Moon(){
+  const moonGeo = new THREE.SphereGeometry(0.1,30,30);
+  const moonMat= new THREE.MeshStandardMaterial()
+  moonMat.map=THREE.ImageUtils.loadTexture(moonColor)
+  const moon = new THREE.Mesh(moonGeo,moonMat);
+  const moonObj = new THREE.Object3D();
 
-  const myMoonMesh=React.useRef();
-
+  moonObj.add(moon);
+  moon.position.x=3.5
+  moon.position.y=1
+  moonObj.rotateX(0.4)
   useFrame(()=>{
-    myMoonMesh.current.rotation.y=0.01;
+    moonObj.rotateY(0.02);
     
   })
-  const moon = useLoader(TextureLoader, moonColor)
-  const moonR = useLoader(TextureLoader, moonRough)
+  
+  
   return(
-          <mesh ref={myMoonMesh} position={[0,1.5,1]}>
-              
-        {/* Width and height segments for displacementMap */}
-        <sphereBufferGeometry args={[0.1, 100, 100]} />
-        <meshStandardMaterial
-        map={moon}
-         roughnessMap={moonR}
-        />
-            </mesh>
+          <primitive object={moonObj}/>
           )
 }
 function Stars(){
@@ -46,25 +56,37 @@ function Stars(){
 
 
   const stars=new THREE.Points(starGeometry,starMaterial)
-
-  for(let i=0;i<1000;i++){
+  const sun = new THREE.PointLight;
+  sun.position.set(0,0,10)
+  for(let i=0;i<10000;i++){
     const x=(Math.random()-0.5)*2000
     const y=(Math.random()-0.5)*2000
     const z=-Math.random()*2000
     starVertices.push(x,y,z)
   }
+  for(let i=0;i<10000;i++){
+    const x=(Math.random()-0.5)*2000
+    const y=(Math.random()-0.5)*2000
+    const z=Math.random()*2000
+    starVertices.push(x,y,z)
+  }
 
+  stars.add(sun)
   starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices,3))
-  
+  useFrame(()=>{
+    stars.rotateY(-0.005)
+    
+  })
   return(
         <primitive object={stars}/>
         )
 }
 function Earth() {
   const myMesh = React.useRef();
+  
   useFrame(() => {
     
-    myMesh.current.rotation.y += 0.01;
+    myMesh.current.rotation.y += 0.0015;
   });
   const name = useLoader(TextureLoader,Color);
   const aob =useLoader(TextureLoader,ao)
@@ -72,18 +94,18 @@ function Earth() {
   return (
     <>
       <ambientLight color={"white"} intensity={0.05}></ambientLight>
-      <pointLight color="white" intensity={1} position={[10, 0,0]} />
+      
       <mesh ref={myMesh}>
         {/* Width and height segments for displacementMap */}
-        <sphereBufferGeometry args={[1, 100, 100]} />
-        <meshStandardMaterial 
+        <sphereBufferGeometry args={[2, 100, 100]} />
+        <meshPhongMaterial 
           
-          displacementScale={0.05}
+          displacementScale={0.15}
           map={name}
           roughnessMap={aob}
           displacementMap={displacementM}
         />
-        <Moon/>
+        
       </mesh>
     </>
   );
@@ -96,6 +118,8 @@ export default function App() {
         <Suspense fallback={null}>
           <Stars/>
           <Earth />
+          <Clouds/>
+          <Moon/>
         </Suspense>
       </Canvas>
     </div>
